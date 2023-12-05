7c69a5ba34f7f7b92c421482353972cc716a121b
mhamadkadiry <mhamad.kadiry1@gmail.com>
2023-12-05 20:12:08 +0100

    display the id of product's category

--- models/Product.js
+++ models/Product.js
@@ -80,7 +80,7 @@ productSchema.virtual("reviews", {
 productSchema.pre(/^find/, function (next) {
   this.populate({
     path: "category",
-    select: "name -_id",
+    select: "name",
   });
   next();
 });
